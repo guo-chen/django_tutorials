fbe5cfaadca871e9aff7419d90c12b72811c1a52
guo-chen <george.guo.chen@outlook.com>
2014-11-25 01:53:43 +0800

    tutorial part 3

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,6 @@ from polls import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       url(r'^$', views.index, name='index'),
                        url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
                        url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
                        url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 
 
 def detail(request, question_id):
