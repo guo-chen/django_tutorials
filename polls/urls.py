ee065c2db52e4ac75906691455c0519a0265669c
George Chen <george.guo.chen@outlook.com>
2014-11-24 19:13:37 +0800

    workplace

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from polls import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^$', views.index, name='index'),
+                       url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+                       url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+                       url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
                        )
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at results of question %s." % question_id
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
