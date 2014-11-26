2d33ba6e8a39a4598d28e6cfd143b684d1662e90
George Chen <george.guo.chen@outlook.com>
2014-11-26 17:58:29 +0800

    Django Documentaion Tutorial, Part 4

--- polls/admin.py
+++ polls/admin.py
@@ -11,7 +11,7 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-        ]
+    ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 from polls.models import Question
