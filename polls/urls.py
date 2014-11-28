83232b9eb35f78131ce032888bed3a730064acc0
guo-chen <george.guo.chen@outlook.com>
2014-11-28 23:24:07 +0800

    PEP8

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,4 @@ urlpatterns = patterns('',
                        url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
                        url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
                        url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-)
+                       )
