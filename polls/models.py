67702faf5c54e2196539d2a96f166d63950dbce0
guo-chen <george.guo.chen@outlook.com>
2014-12-03 01:15:37 +0800

    Now it is able to print Chinese polls and votes name

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
+    def __unicode__(self):
         return self.question_text
 
     def was_published_recently(self):
@@ -24,5 +24,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.choice_text
